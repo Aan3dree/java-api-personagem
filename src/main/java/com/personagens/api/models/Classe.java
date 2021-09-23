8a0abc0a0d36ebbb8508df66e7075fe99c0dcbf5
Carlos Alberto Jr <carlosalbertobzjr@gmail.com>
2021-09-23 20:55:05 -0300

    Removendo tag git

--- src/main/java/com/personagens/api/models/Classe.java
+++ src/main/java/com/personagens/api/models/Classe.java
@@ -1,5 +1,27 @@
 package com.personagens.api.models;
 
-public class Classe {
+import javax.persistence.Entity;
 
+@Entity(name = "TB_CLASSE")
+public class Classe extends EntidadeAbstrata {
+
+	
+	private String nome;
+	private String pericias;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getPericias() {
+		return pericias;
+	}
+	public void setPericias(String pericias) {
+		this.pericias = pericias;
+	}
+	
+	
+	
 }
--- src/main/java/com/personagens/api/models/Habilidade.java
+++ src/main/java/com/personagens/api/models/Habilidade.java
@@ -1,5 +1,18 @@
 package com.personagens.api.models;
 
-public class Habilidade {
+import javax.persistence.Entity;
+
+@Entity(name= "TB_HABILIDADES")
+public class Habilidade extends EntidadeAbstrata {
+	
+	private String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
 }
+
